5725d1579f2dcf7f4e6e812b213e0ea4c3913f18
Zolhungaj <zolhungaj@gmail.com>
2023-01-07 22:31:09 +0100

    initial commit

---
+++ src/test/java/tech/zolhungaj/amqcontestbot/AmqContestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zolhungaj.amqcontestbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqContestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
