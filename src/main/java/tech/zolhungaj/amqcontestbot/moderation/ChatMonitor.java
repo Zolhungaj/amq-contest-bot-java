650e860285c19efc76131af08a10fac23673e7d0
Zolhungaj <zolhungaj@gmail.com>
2023-02-26 18:56:48 +0100

    add player-name checking to ChatMonitor

--- src/main/java/tech/zolhungaj/amqcontestbot/moderation/ChatMonitor.java
+++ src/main/java/tech/zolhungaj/amqcontestbot/moderation/ChatMonitor.java
@@ -1,43 +1,81 @@
 package tech.zolhungaj.amqcontestbot.moderation;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tech.zolhungaj.amqapi.servercommands.gameroom.GameChatMessage;
 import tech.zolhungaj.amqapi.servercommands.gameroom.GameChatUpdate;
+import tech.zolhungaj.amqapi.servercommands.gameroom.NewPlayer;
+import tech.zolhungaj.amqapi.servercommands.gameroom.SpectatorJoined;
 import tech.zolhungaj.amqcontestbot.ApiManager;
 
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ChatMonitor {
 
-    private final Set<BannedPhrase> bannedPhrases;
+    private final ApiManager api;
+    private final NameResolver nameResolver;
     private final PunishmentManager punishmentManager;
+    private Set<BannedPhrase> bannedPhrases;
 
-    public ChatMonitor(@Autowired ApiManager api, @Autowired PunishmentManager punishmentManager){
-        this.punishmentManager = punishmentManager;
+    @PostConstruct
+    public void init(){
         bannedPhrases = Set.of();
         api.on(command -> {
             if(command instanceof GameChatMessage gameChatMessage){
                 this.handleMessage(gameChatMessage);
-            }else if (command instanceof GameChatUpdate gameChatUpdate){
+            }
+            if (command instanceof GameChatUpdate gameChatUpdate){
                 gameChatUpdate.messages().forEach(this::handleMessage);
             }
+            if (command instanceof NewPlayer newPlayer){
+                handlePlayer(newPlayer.playerName());
+            }
+            if (command instanceof SpectatorJoined spectatorJoined){
+                handlePlayer(spectatorJoined.playerName());
+            }
+            //TODO: check answers too
             return true;
         });
     }
 
     private void handleMessage(GameChatMessage gameChatMessage){
-        String message = gameChatMessage.message();
-        for(BannedPhrase phrase : bannedPhrases){
-            if(phrase.pattern.matcher(message).find()){
-                punishmentManager.kick(gameChatMessage.sender());
-                //TODO: report once API has it ready
-            }
+        rateMessage(gameChatMessage.sender(), gameChatMessage.message());
+    }
+
+    private void handlePlayer(String nickname){
+        rateName(nickname, nickname);
+        nameResolver.getTrueName(nickname).thenAccept(trueName -> rateName(nickname, trueName));
+    }
+
+    private void rateMessage(String sender, String message){
+        Set<String> reasons = findBannedPhrases(message);
+        if(!reasons.isEmpty()){
+            punishmentManager.kick(sender);
         }
+        //reasons.forEach();
+        //TODO: report for each violation in message
+    }
+
+    private void rateName(String nickname, String name){
+        Set<String> reasons = findBannedPhrases(name);
+        if(!reasons.isEmpty()){
+            punishmentManager.kick(nickname);
+            //TODO: report for "Offensive Name"
+        }
+    }
+
+    private Set<String> findBannedPhrases(String content){
+        return bannedPhrases.stream()
+                .filter(phrase -> phrase.pattern.matcher(content).find())
+                .map(BannedPhrase::reason)
+                .collect(Collectors.toSet());
     }
 
     private record BannedPhrase(Pattern pattern, String reason){
