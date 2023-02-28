179622e1eed82014371c94bfd141f42175e004a5
Zolhungaj <zolhungaj@gmail.com>
2023-02-28 23:49:41 +0100

    create MessageService as bridge to database for i18n and censoring

--- src/main/java/tech/zolhungaj/amqcontestbot/chat/ChatController.java
+++ src/main/java/tech/zolhungaj/amqcontestbot/chat/ChatController.java
@@ -18,30 +18,22 @@ import static tech.zolhungaj.amqcontestbot.Util.chunkMessageToFitLimits;
 public class ChatController {
     public static final int MESSAGE_LIMIT = 150;
     private final ApiManager api;
+    private final MessageService messageService;
     private final ConcurrentLinkedQueue<String> pendingMessages = new ConcurrentLinkedQueue<>();
 
     public List<String> send(String i18nCanonicalName, Object... arguments){
-        return this.send(i18nCanonicalName, List.of(arguments));
+        return send(i18nCanonicalName, List.of(arguments));
     }
     public List<String> send(String i18nCanonicalName, List<Object> arguments){
-        log.debug("Start replacement {}", i18nCanonicalName);
-        String message = getMessageForCanonicalName(i18nCanonicalName);
-        for(var i = 0; i < arguments.size(); i++){
-            String search = "%" + (i+1) + "%";
-            message = message.replaceAll(search, arguments.get(i).toString());
-        }
+        String message = messageService.getMessage(i18nCanonicalName, arguments);
         return sendRaw(message);
     }
     public List<String> sendRaw(String message){
-        String censoredMessage = selfCensor(message);
+        String censoredMessage = messageService.censor(message);
         final List<String> messageChunked = chunkMessageToFitLimits(censoredMessage, MESSAGE_LIMIT);
         log.info("Sending chat '{}'", censoredMessage);
         pendingMessages.addAll(messageChunked);
         return messageChunked;
-    }
-
-    private String getMessageForCanonicalName(String i18nCanonicalName){
-        return i18nCanonicalName + "%1% %2% %3% %4% %5% %6% %7%";//TODO: implement once database is done
     }
 
     @Scheduled(fixedDelay = 500)
@@ -50,9 +42,5 @@ public class ChatController {
         if(nextMessage != null){
             api.sendCommand(SendMessage.builder().message(nextMessage).build());
         }
-    }
-
-    private String selfCensor(String message){
-        return message.replace("ROBOT", "MACHINE");//TODO:implement censoring preventing certain phrases from being spoken
     }
 }
---
+++ src/main/java/tech/zolhungaj/amqcontestbot/chat/MessageService.java
@@ -0,0 +1,28 @@
+package tech.zolhungaj.amqcontestbot.chat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class MessageService {
+
+    public String getMessage(String i18nCanonicalName, List<Object> arguments){
+        log.debug("Start replacement {}", i18nCanonicalName);
+        String message = getMessageForCanonicalName(i18nCanonicalName);
+        for(var i = 0; i < arguments.size(); i++){
+            String search = "%" + (i+1) + "%";
+            message = message.replaceAll(search, arguments.get(i).toString());
+        }
+        return message;
+    }
+    private String getMessageForCanonicalName(String i18nCanonicalName){
+        return i18nCanonicalName + "%1% %2% %3% %4% %5% %6% %7%";//TODO: implement once database is done
+    }
+
+    public String censor(String message){
+        return message.replace("ROBOT", "MACHINE");//TODO:implement censoring preventing certain phrases from being spoken, once database is done
+    }
+}
