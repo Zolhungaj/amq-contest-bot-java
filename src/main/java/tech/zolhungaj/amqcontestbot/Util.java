5f40ffb2737f3756af67950d197efe5e6a6beb3b
Zolhungaj <zolhungaj@gmail.com>
2023-02-28 23:24:19 +0100

    pull chunkMessageToLimits out of ChatController
    It will be also be used by the DM module, and is also easier to read now.

--- src/main/java/tech/zolhungaj/amqcontestbot/Util.java
+++ src/main/java/tech/zolhungaj/amqcontestbot/Util.java
@@ -1,6 +1,10 @@
 package tech.zolhungaj.amqcontestbot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Util {
     private static final Pattern GUEST_MATCHER = Pattern.compile("^Guest-\\d{5}$");
@@ -8,5 +12,48 @@ public class Util {
 
     public static boolean isGuest(String playerName){
         return GUEST_MATCHER.matcher(playerName).matches();
+    }
+
+    public static List<String> chunkMessageToFitLimits(String message, int messageLimit){
+        if(message.length() <= messageLimit){
+            return List.of(message);
+        }else{
+            //split into words that can fit inside the message limit
+            final String splitter = " ";
+            List<String> messageSplit = Stream
+                    .of(message.split(splitter))
+                    .flatMap(chunkWordToLimitsFunction(messageLimit))
+                    .toList();
+            List<String> chunks = new ArrayList<>();
+            final StringBuilder builder = new StringBuilder();
+            for(String word : messageSplit){
+                if(builder.length() + splitter.length() + word.length() <= messageLimit){
+                    builder.append(splitter);
+                    builder.append(word);
+                }else{
+                    chunks.add(builder.toString());
+                    builder.delete(0, messageLimit);
+                }
+            }
+            if(!builder.isEmpty()){
+                chunks.add(builder.toString());
+            }
+            return List.copyOf(chunks);
+        }
+    }
+
+    private static Function<String, Stream<String>> chunkWordToLimitsFunction(int messageLimit){
+        return word -> chunkWordToLimits(word, messageLimit);
+    }
+
+    private static Stream<String> chunkWordToLimits(String word, int messageLimit){
+        Stream.Builder<String> chunks = Stream.builder();
+        while(word.length() > messageLimit){
+            String left = word.substring(0,messageLimit);
+            chunks.add(left);
+            word = word.substring(messageLimit);
+        }
+        chunks.add(word);
+        return chunks.build();
     }
 }
--- src/main/java/tech/zolhungaj/amqcontestbot/chat/ChatController.java
+++ src/main/java/tech/zolhungaj/amqcontestbot/chat/ChatController.java
@@ -1,26 +1,24 @@
 package tech.zolhungaj.amqcontestbot.chat;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tech.zolhungaj.amqapi.clientcommands.lobby.SendMessage;
 import tech.zolhungaj.amqcontestbot.ApiManager;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.stream.Stream;
+
+import static tech.zolhungaj.amqcontestbot.Util.chunkMessageToFitLimits;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ChatController {
-    private static final int MESSAGE_LIMIT = 150;
+    public static final int MESSAGE_LIMIT = 150;
     private final ApiManager api;
     private final ConcurrentLinkedQueue<String> pendingMessages = new ConcurrentLinkedQueue<>();
-    public ChatController(@Autowired ApiManager api){
-        this.api = api;
-    }
 
     public List<String> send(String i18nCanonicalName, Object... arguments){
         return this.send(i18nCanonicalName, List.of(arguments));
@@ -35,43 +33,9 @@ public class ChatController {
         return sendRaw(message);
     }
     public List<String> sendRaw(String message){
-        message = selfCensor(message);
-        final List<String> messageChunked;
-        if(message.length() <= MESSAGE_LIMIT){
-            messageChunked = List.of(message);
-        }else{
-            //split into words that can fit inside the message limit
-            final String splitter = " ";
-            List<String> messageSplit = Stream
-                    .of(message.split(splitter))
-                    .flatMap(word -> {
-                        List<String> chunks = new ArrayList<>();
-                        while(word.length() > MESSAGE_LIMIT){
-                            String left = word.substring(0,MESSAGE_LIMIT);
-                            chunks.add(left);
-                            word = word.substring(MESSAGE_LIMIT);
-                        }
-                        chunks.add(word);
-                        return chunks.stream();
-                    })
-                    .toList();
-            List<String> chunks = new ArrayList<>();
-            final StringBuilder builder = new StringBuilder();
-            for(String word : messageSplit){
-                if(builder.length() + splitter.length() + word.length() <= MESSAGE_LIMIT){
-                    builder.append(splitter);
-                    builder.append(word);
-                }else{
-                    chunks.add(builder.toString());
-                    builder.delete(0, MESSAGE_LIMIT);
-                }
-            }
-            if(!builder.isEmpty()){
-                chunks.add(builder.toString());
-            }
-            messageChunked = List.copyOf(chunks);
-        }
-        log.info("Sending chat '{}'", message);
+        String censoredMessage = selfCensor(message);
+        final List<String> messageChunked = chunkMessageToFitLimits(censoredMessage, MESSAGE_LIMIT);
+        log.info("Sending chat '{}'", censoredMessage);
         pendingMessages.addAll(messageChunked);
         return messageChunked;
     }
