3ab7fda9cd9cded89df70b94f87fdc7109e76ddb
Zolhungaj <zolhungaj@gmail.com>
2023-03-14 00:23:55 +0100

    add the waiting loop with status messages to LobbyManager

--- src/main/java/tech/zolhungaj/amqcontestbot/room/LobbyManager.java
+++ src/main/java/tech/zolhungaj/amqcontestbot/room/LobbyManager.java
@@ -16,6 +16,7 @@ import tech.zolhungaj.amqapi.servercommands.globalstate.LoginComplete;
 import tech.zolhungaj.amqapi.servercommands.objects.Player;
 import tech.zolhungaj.amqapi.sharedobjects.gamesettings.GameSettings;
 import tech.zolhungaj.amqcontestbot.ApiManager;
+import tech.zolhungaj.amqcontestbot.chat.ChatController;
 import tech.zolhungaj.amqcontestbot.gamemode.GameMode;
 import tech.zolhungaj.amqcontestbot.gamemode.MasterOfTheSeasonsGameMode;
 
@@ -32,10 +33,13 @@ import java.util.stream.Collectors;
 @EnableScheduling
 @RequiredArgsConstructor
 public class LobbyManager {
+    private static final int WAIT_TIME = 30;
+    private static final int SECONDARY_WAIT_TIME = 10;
     private final ApiManager api;
+    private final ChatController chatController;
     private final GameMode gameMode = new MasterOfTheSeasonsGameMode();
     private GameSettings currentSettings = null;
-    private long counter = 0L;
+    private int counter = 0;
     private String selfName = "";
     private boolean inLobby = false;
     private final Map<Integer, LobbyPlayer> players = new ConcurrentHashMap<>();
@@ -65,8 +69,9 @@ public class LobbyManager {
                 int gamePlayerId = toSpectator.playerDescription().gamePlayerId().orElse(-1);
                 removePlayer(gamePlayerId);
             }else if(command instanceof PlayerReadyChange playerReadyChange){
-                this.players.computeIfPresent(playerReadyChange.gamePlayerId(), (key, player) -> player.withReady(player.isReady()));
+                this.players.computeIfPresent(playerReadyChange.gamePlayerId(), (key, player) -> player.withReady(playerReadyChange.ready()));
             }
+            //TODO: trigger lock if Catbox is down
             return true;
         });
     }
@@ -101,9 +106,24 @@ public class LobbyManager {
     @Scheduled(fixedRate = 1, timeUnit = TimeUnit.SECONDS)
     public void updateState(){
         if(!inLobby){
+            counter = 0;
             return;
         }
+        if(playerCount() == 0){
+            counter = 0;
+            return;
+        }
+        if(startIfPossible()){
+            return;
+        }
+        sendRelevantMessage();
         counter++;
+    }
+
+    private long playerCount(){
+        return players.values().stream()
+                .filter(LobbyPlayer::inLobby)
+                .count();
     }
 
     private Set<LobbyPlayer> notReadyPlayers(){
@@ -111,5 +131,46 @@ public class LobbyManager {
                 .filter(LobbyPlayer::inLobby)
                 .filter(Predicate.not(LobbyPlayer::isReady))
                 .collect(Collectors.toSet());
+    }
+
+    private boolean startIfPossible(){
+        Set<LobbyPlayer> notReady = notReadyPlayers();
+        if(counter >= WAIT_TIME && notReady.isEmpty()){
+            this.start();
+            return true;
+        }
+        if(counter >= WAIT_TIME + SECONDARY_WAIT_TIME){
+            this.start();
+            return true;
+        }
+        return false;
+    }
+
+    private void start(){
+        counter = 0;
+        chatController.send("lobby.starting");
+        //TODO: start
+    }
+
+    private void sendRelevantMessage(){
+        if(counter < WAIT_TIME){
+            int diff = WAIT_TIME - counter;
+            if(diff % 10 == 0){
+                chatController.send("lobby.countdown.major", diff);
+            }
+            if(diff <= 5){
+                chatController.send("lobby.countdown.final", diff);
+            }
+        }else if(counter == WAIT_TIME){
+            Set<LobbyPlayer> notReady = notReadyPlayers();
+            String message = notReady.stream()
+                    .map(LobbyPlayer::playerName)
+                    .map("@"::concat) //prefix each name with @ to trigger mention
+                    .collect(Collectors.joining(" "));
+            chatController.send("lobby.get-ready", message, SECONDARY_WAIT_TIME);
+        }else{
+            int diff = WAIT_TIME + SECONDARY_WAIT_TIME - counter;
+            chatController.send("lobby.countdown.secondary", diff);
+        }
     }
 }
